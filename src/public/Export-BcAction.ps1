23f1b57d3ed20ded4c94fc8f1a28b3f570b9f6c0
Anthony Howell <anthony@howell-it.com>
2022-07-28 07:21:53 -0700

    fixing os and outpath

--- src/public/Export-BcAction.ps1
+++ src/public/Export-BcAction.ps1
@@ -14,9 +14,9 @@ Function Export-BcAction {
                 DefaultParameters          = $null -ne $json.DefaultParameters ? $json.DefaultParameters : $null
                 IncludeParametersParameter = $null -ne $json.IncludeParametersParameter ? $json.IncludeParametersParameter : $null
                 RedirectCommandOutput      = $null -ne $json.RedirectCommandOutput ? $json.RedirectCommandOutput : $null
-                Windows                    = $null -ne $json.Windows ? $json.Windows : $null
-                Linux                      = $null -ne $json.Linux ? $json.Linux : $null
-                OutPath                    = "C:\tmp\action-$($action.Name)"
+                Windows                    = $json.OperatingSystems -contains 'Windows'
+                Linux                      = $json.OperatingSystems -contains 'Linux'
+                OutPath                    = $OutPath
             }
             New-BcOsCommandAction @splat
         }
