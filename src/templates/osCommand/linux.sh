49175a3689587237f1341fa155f3efbbaede68f7
Anthony Howell <anthony@howell-it.com>
2022-07-28 07:54:21 -0700

    adding linux template script

---
+++ src/templates/osCommand/linux.sh
@@ -0,0 +1,34 @@
+# check for current package manager
+declare -A osInfo;
+osInfo[/etc/redhat-release]=yum
+osInfo[/etc/arch-release]=pacman
+osInfo[/etc/gentoo-release]=emerge
+osInfo[/etc/SuSE-release]=zypp
+osInfo[/etc/debian_version]=apt-get
+osInfo[/etc/alpine-release]=apk
+
+for f in ${!osInfo[@]}
+do
+    if [[ -f $f ]];then
+        #echo Package manager: ${osInfo[$f]}
+        pman=${osInfo[$f]}
+    fi
+done
+
+# check if jq is installed
+if ! [ -x "$(command -v jq)" ]; then
+    echo "Installing jq"
+
+    # check for sudo, install
+    if [ -x "$(command -v sudo)" ]; then
+        sudo $pman install jq
+    else
+        $pman install jq
+    fi
+else
+    echo "jq already installed"
+fi
+
+{ jq }
+
+{ if }
