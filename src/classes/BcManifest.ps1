8a35af5b4b442830727e781a548747b3f32c7c3c
Anthony Howell <anthony@howell-it.com>
2022-07-28 08:52:15 -0700

    fixing commands

--- src/classes/BcManifest.ps1
+++ src/classes/BcManifest.ps1
@@ -5,15 +5,15 @@ class BcManifest {
 
     BcManifest() {
         $this.Header = 'COPY . .'
-        $this.WindowsCommand = '"PowerShell.exe -ExecutionPolicy Bypass -File .\windows\script.ps1"'
-        $this.LinuxCommand = 'linux/script.sh'
+        $this.WindowsCommand = 'RUN_WIN "PowerShell.exe -ExecutionPolicy Bypass -File .\windows\script.ps1"'
+        $this.LinuxCommand = 'RUN_LIN linux/script.sh'
     }
 
     Export(
         [string]$Path,
         [bool]$Force
     ) {
-        $str = "$($this.Header)`n$($this.WindowsCommand)`n$($this.WindowsCommand)"
+        $str = "$($this.Header)`n$($this.WindowsCommand)`n$($this.LinuxCommand)"
         if ((Test-Path $path) -and -not $Force) {
             Throw 'File already exists. Use $Force to overwrite.'
         } else {
