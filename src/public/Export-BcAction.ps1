47e40335e614625d3924e0da9e0aac1c7a809bf4
Anthony Howell <anthony@howell-it.com>
2022-07-28 08:51:33 -0700

    supporting an array

--- src/public/Export-BcAction.ps1
+++ src/public/Export-BcAction.ps1
@@ -4,24 +4,27 @@ Function Export-BcAction {
         [string]$ConfigPath,
         [string]$OutPath
     )
-    $json = Get-Content $ConfigPath | ConvertFrom-Json -AsHashtable
-    switch ($json.ActionType) {
-        'OSCommand' {
-            $splat = @{
-                Name                       = $null -ne $json.Name ? $json.Name : $null
-                Command                    = $null -ne $json.Command ? $json.Command : $null
-                ActionParameters           = $null -ne $json.ActionParameters ? $json.ActionParameters : $null
-                DefaultParameters          = $null -ne $json.DefaultParameters ? $json.DefaultParameters : $null
-                IncludeParametersParameter = $null -ne $json.IncludeParametersParameter ? $json.IncludeParametersParameter : $null
-                RedirectCommandOutput      = $null -ne $json.RedirectCommandOutput ? $json.RedirectCommandOutput : $null
-                Windows                    = $json.OperatingSystems -contains 'Windows'
-                Linux                      = $json.OperatingSystems -contains 'Linux'
-                OutPath                    = $OutPath
+    [hashtable[]]$json = Get-Content $ConfigPath | ConvertFrom-Json -AsHashtable
+    foreach ($actionHt in $json) {
+        $action = switch ($json.ActionType) {
+            'OSCommand' {
+                $splat = @{
+                    Name                       = $null -ne $actionHt.Name ? $actionHt.Name : $null
+                    Command                    = $null -ne $actionHt.Command ? $actionHt.Command : $null
+                    ActionParameters           = $null -ne $actionHt.ActionParameters ? $actionHt.ActionParameters : $null
+                    DefaultParameters          = $null -ne $actionHt.DefaultParameters ? $actionHt.DefaultParameters : $null
+                    IncludeParametersParameter = $null -ne $actionHt.IncludeParametersParameter ? $actionHt.IncludeParametersParameter : $null
+                    RedirectCommandOutput      = $null -ne $actionHt.RedirectCommandOutput ? $actionHt.RedirectCommandOutput : $null
+                    Windows                    = $actionHt.OperatingSystems -contains 'Windows'
+                    Linux                      = $actionHt.OperatingSystems -contains 'Linux'
+                    OutPath                    = $OutPath
+                }
+                New-BcOsCommandAction @splat
             }
-            New-BcOsCommandAction @splat
+            default {
+                Write-Warning "Unsupported ActionType"
+            }
         }
-        default {
-            Write-Warning "Unsupported ActionType"
-        }
+        $action.Export("$OutPath\$($actionHt.Name)")
     }
 }
