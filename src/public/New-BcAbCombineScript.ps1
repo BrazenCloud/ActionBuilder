4614d26d5252c17ba45dc27c65b1d0e778f24c5b
Anthony Howell <anthony@howell-it.com>
2022-08-10 07:55:45 -0700

    different template approach

--- src/public/New-BcAbCombineScript.ps1
+++ src/public/New-BcAbCombineScript.ps1
@@ -9,18 +9,7 @@ Function New-BcAbCombineScript {
         [string]$DefaultParameters
     )
 
-    $templates = @{
-        'Windows' = @{
-            IfCombine = Get-Template -Name osCommand-WindowsIfCombine
-            IfParam   = Get-Template -Name osCommand-WindowsIfParam
-            Else      = Get-Template -Name osCommand-WindowsElse
-        }
-        'Linux'   = @{
-            IfCombine = Get-Template -Name osCommand-LinuxIfCombine
-            IfParam   = Get-Template -Name osCommand-LinuxIfParam
-            Else      = Get-Template -Name osCommand-LinuxElse
-        }
-    }
+    $templates = Get-Template -All
 
     $orStatement = @{
         Windows = ' -or '
@@ -34,15 +23,15 @@ Function New-BcAbCombineScript {
         Parameters = $DefaultParameters
     }
 
-    $mainIf = $templates[$OperatingSystem]['IfCombine'] -replace '"?\{exists\}"?', ($Parameters.GetIsEmptyStatement($OperatingSystem) -join $orStatement[$OperatingSystem])
+    $mainIf = $templates[$OperatingSystem]['if']['combine'] -replace '\{exists\}', ($Parameters.GetIsEmptyStatement($OperatingSystem) -join $orStatement[$OperatingSystem])
 
     $ifArr = foreach ($param in $Parameters | Where-Object { $_.Name -ne 'Parameters' }) {
-        ($templates[$OperatingSystem]['IfParam'] -replace '\{param\}', $Param.Name) -replace '"?\{value\}"?', "`"$($param.GetValue($OperatingSystem))`""
+        ($templates[$OperatingSystem]['if']['param'] -replace '\{param\}', $Param.Name) -replace '\{value\}', "`"$($param.GetValue($OperatingSystem))`""
     }
 
     $mcSplat.Parameters = $DefaultParameters
 
-    (($mainIf -replace '"?\{if\}"?', ($ifArr -join "`n"))) `
-        -replace '"?\{command\}"?', $Command `
-        -replace '"?\{else\}"?', (($templates[$OperatingSystem]['Else'] -replace '"?\{action\}"?', (makeCommand @mcSplat)) -join "`n")
+    (($mainIf -replace '\{if\}', ($ifArr -join "`n"))) `
+        -replace '\{command\}', $Command `
+        -replace '\{else\}', (($templates[$OperatingSystem]['if']['ifElse'] -replace '\{action\}', (makeCommand @mcSplat)) -join "`n")
 }
