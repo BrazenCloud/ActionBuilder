3f0fe482129161f9a055e798ffd677869f104c97
Anthony Howell <anthony@howell-it.com>
2022-08-10 09:14:56 -0700

    dynamically building the parameter splat

--- src/public/Export-BcAction.ps1
+++ src/public/Export-BcAction.ps1
@@ -7,26 +7,12 @@ Function Export-BcAction {
     )
     [hashtable[]]$json = Get-Content $ConfigPath | ConvertFrom-Json -AsHashtable
     foreach ($actionHt in $json) {
-        $action = switch ($json.ActionType) {
-            'OSCommand' {
-                $splat = @{
-                    Name                       = $null -ne $actionHt.Name ? $actionHt.Name : $null
-                    Description                = $null -ne $actionHt.Description ? $actionHt.Description : $null
-                    Command                    = $null -ne $actionHt.Command ? $actionHt.Command : $null
-                    ActionParameters           = $null -ne $actionHt.ActionParameters ? $actionHt.ActionParameters : $null
-                    DefaultParameters          = $null -ne $actionHt.DefaultParameters ? $actionHt.DefaultParameters : $null
-                    IncludeParametersParameter = $null -ne $actionHt.IncludeParametersParameter ? $actionHt.IncludeParametersParameter : $null
-                    RedirectCommandOutput      = $null -ne $actionHt.RedirectCommandOutput ? $actionHt.RedirectCommandOutput : $null
-                    Windows                    = $actionHt.OperatingSystems -contains 'Windows'
-                    Linux                      = $actionHt.OperatingSystems -contains 'Linux'
-                    OutPath                    = $OutPath
-                }
-                New-BcOsCommandAction @splat
-            }
-            default {
-                Write-Warning "Unsupported ActionType"
-            }
+        $h = Get-Help New-BcAbAction
+        $splat = @{}
+        foreach ($parameter in $h.parameters.parameter.Name) {
+            $splat[$parameter] = $null -ne $actionHt.$parameter ? $actionHt.$parameter : $null
         }
+        $action = New-BcAbAction @splat
         if ($ClearOutputFolders.IsPresent) {
             if (Test-Path "$OutPath\$($actionHt.Name)") {
                 Get-ChildItem "$OutPath\$($actionHt.Name)" -Recurse -File | ForEach-Object {
