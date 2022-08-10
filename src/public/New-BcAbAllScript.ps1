8b7881b95adfae992189c62deaeca9ff334443f8
Anthony Howell <anthony@howell-it.com>
2022-08-10 07:54:24 -0700

    different template approach

--- src/public/New-BcAbAllScript.ps1
+++ src/public/New-BcAbAllScript.ps1
@@ -9,27 +9,7 @@ Function New-BcAbAllScript {
         [string]$DefaultParameters
     )
 
-    $templates = @{
-        'Windows' = @{
-            IfCombine = Get-Template -Name osCommand-WindowsIfCombine
-            IfParam   = Get-Template -Name osCommand-WindowsIfParam
-            Else      = Get-Template -Name osCommand-WindowsElse
-            BoolIf    = Get-Template -Name osCommand-WindowsIfBool
-            IfString  = Get-Template -Name osCommand-WindowsIfString
-        }
-        'Linux'   = @{
-            IfCombine = Get-Template -Name osCommand-LinuxIfCombine
-            IfParam   = Get-Template -Name osCommand-LinuxIfParam
-            Else      = Get-Template -Name osCommand-LinuxElse
-            BoolIf    = Get-Template -Name osCommand-LinuxIfBool
-            IfString  = Get-Template -Name osCommand-LinuxIfString
-        }
-    }
-
-    $orStatement = @{
-        Windows = ' -or '
-        Linux   = ' || '
-    }
+    $templates = Get-Template -All
 
     $mcSplat = @{
         Command    = $Command
@@ -42,10 +22,10 @@ Function New-BcAbAllScript {
         # if this param has a default value, use it, else it must have come from the passed actionParameters var
         if ($param.Type -eq 2) {
             $mcSplat.Parameters = $param.GetValue($OperatingSystem)
-            $templates[$OperatingSystem]['BoolIf'].Replace('{param}', $Param.Name) -replace '"?{command}"?', (makeCommand @mcSplat)
+            $templates[$OperatingSystem]['if']['bool'].Replace('{param}', $Param.Name) -replace '"?{command}"?', (makeCommand @mcSplat)
         } elseif ($Param.Type -eq 0) {
             $mcSplat.Parameters = $param.GetValue($OperatingSystem)
-            $templates[$OperatingSystem]['IfString'].Replace('{param}', $Param.Name) -replace '"?{command}"?', (makeCommand @mcSplat)
+            $templates[$OperatingSystem]['if']['string'].Replace('{param}', $Param.Name) -replace '{command}', (makeCommand @mcSplat)
         }
     }
 }
