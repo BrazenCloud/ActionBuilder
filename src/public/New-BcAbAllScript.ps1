a5986368a4cd46aedacd5f414aa5e3f65fbf6fe4
Anthony Howell <anthony@howell-it.com>
2022-08-11 13:54:23 -0700

    fixing default command in all

--- src/public/New-BcAbAllScript.ps1
+++ src/public/New-BcAbAllScript.ps1
@@ -18,7 +18,7 @@ Function New-BcAbAllScript {
         Parameters = $DefaultParameters
     }
 
-    foreach ($param in $Parameters) {
+    foreach ($param in ($Parameters | Where-Object { $_.Name -ne 'Parameters' })) {
         # if this param has a default value, use it, else it must have come from the passed actionParameters var
         if ($param.Type -eq 2) {
             $mcSplat.Parameters = $param.GetValue($OperatingSystem)
@@ -27,5 +27,9 @@ Function New-BcAbAllScript {
             $mcSplat.Parameters = $param.GetValue($OperatingSystem)
             $templates[$OperatingSystem]['if']['string'].Replace('{param}', $Param.Name) -replace '{command}', (makeCommand @mcSplat)
         }
+    }
+    $Parameters | Where-Object { $_.Name -eq 'Parameters' } | ForEach-Object {
+        $mcSplat.Parameters = $_.GetValue($OperatingSystem)
+        makeCommand @mcSplat
     }
 }
